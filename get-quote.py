adb8df5d8d5fff8adc9275f4b800282f5c022bfe
ciatstudent <74837048+ciatstudent@users.noreply.github.com>
2021-03-17 21:02:28 -0700

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
 def Primary():
    print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes)
 
 if __name__== "__Primary__":
   Primary()
