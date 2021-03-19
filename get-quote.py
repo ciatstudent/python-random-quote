012435141d75245634b4f6b5792614f54228b6f8
ciatstudent <74837048+ciatstudent@users.noreply.github.com>
2021-03-18 22:53:20 -0700

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
-def Main():
-   print("Keep it logically awesome.")
+
+import random
+def main():
+  print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
+  last = 13
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 
-  print(quotes[0])
-
-if __name__== "__Main__":
-  Main()
+if __name__== "__main__":
+  main()
